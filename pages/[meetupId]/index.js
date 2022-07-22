e4a50972d9b9f827cabd27669333c835dc4ed2dc
dduicoder <sijinddui2@gmail.com>
2022-07-22 21:17:59 +0900

    error

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -34,7 +34,7 @@ export const getStaticPaths = async () => {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
